da4c31120c2011418beada26a2b22fdfe9143a74
Ummay-Samiun-Basri <141258544+Ummay-Samiun-Basri@users.noreply.github.com>
2025-01-26 01:59:24 +0600

    Add files via upload

---
+++ Python_C201279/mypackage/module1.py
@@ -0,0 +1,4 @@
+
+def greet_user(name):
+    """Function to greet the user."""
+    return f"Hello, {name}! Welcome to our package."
---
+++ Python_C201279/mypackage/module2.py
@@ -0,0 +1,4 @@
+
+def calculate_square(number):
+    """Function to calculate the square of a number."""
+    return number ** 2
